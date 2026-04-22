809e78053dfbcb5c3c61a1cb61e90327633b0029
Nahiara Schiaffino <nahiara.schiaffino@alu.frt.utn.edu.ar>
2026-04-22 15:36:58 -0300

    Correccion de errores de calcularConsumo.

--- Dsw2026Ej5/Domain/VehiculoCombustible.cs
+++ Dsw2026Ej5/Domain/VehiculoCombustible.cs
@@ -28,6 +28,11 @@ public class VehiculoCombustible: Vehiculo
 
     public override double CalcularConsumo(double kilometros)
     {
-        return kilometros * kilometrosPorLitro;
+        double litros = kilometros / kilometrosPorLitro;
+        if (DateTime.Now.Year - GetAnio() > 5)
+        {
+            litros += (kilometros / 15) * litrosExtra;
+        }
+        return litros;
     }
 }
